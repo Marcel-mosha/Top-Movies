2e6bad577dda521838a063dbc85330c40303b059
Marcel-mosha <marcellmosha84@gmail.com>
2025-06-19 22:39:20 +0300

    Enhance styling in all pages and use postgres database instead of sqlite

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from wtforms.validators import DataRequired
 import requests
 import os
 from dotenv import load_dotenv
+from urllib.parse import quote
 
 load_dotenv()
 app = Flask(__name__)
@@ -17,6 +18,11 @@ Bootstrap5(app)
 api_key = os.environ.get("API_KEY")
 movie_endpoint = os.environ.get("MOVIE_ENDPOINT")
 movie_db_img_url = "https://image.tmdb.org/t/p/w500"
+db_user = os.environ.get("DB_USER")
+database = os.environ.get("DB")
+db_password = quote(os.environ.get("DB_PASSWORD"))
+db_port = os.environ.get("DB_PORT")
+db_host = os.environ.get("DB_HOST")
 
 
 class MovieForm(FlaskForm):
@@ -35,7 +41,8 @@ class Base(DeclarativeBase):
     pass
 
 
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///movies.db"
+app.config['SQLALCHEMY_DATABASE_URI'] = f"postgresql://{db_user}:{db_password}@{db_host}:{db_port}/{database}"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
@@ -55,47 +62,17 @@ with app.app_context():
     db.create_all()
 
 
-# new_movie = Movie(
-#     title="Phone Booth",
-#     year=2002,
-#     description="Publicist Stuart Shepard finds himself trapped in a phone booth, pinned down by an extortionist's "
-#                 "sniper rifle. Unable to leave or receive outside help, Stuart's negotiation with the caller "
-#                 "leads to"
-#                 "a jaw-dropping climax.",
-#     rating=7.3,
-#     ranking=10,
-#     review="My favourite character was the caller.",
-#     img_url="https://image.tmdb.org/t/p/w500/tjrX2oWRCM3Tvarz38zlZM7Uc10.jpg"
-# )
-#
-# with app.app_context():
-#     db.session.add(new_movie)
-#     db.session.commit()
-#
-# second_movie = Movie(
-#     title="Avatar The Way of Water",
-#     year=2022,
-#     description="Set more than a decade after the events of the first film, learn the story of the Sully family ("
-#                 "Jake, Neytiri, and their kids), the trouble that follows them, the lengths they go to keep each "
-#                 "other safe, the battles they fight to stay alive, and the tragedies they endure.",
-#     rating=7.3,
-#     ranking=9,
-#     review="I liked the water.",
-#     img_url="https://image.tmdb.org/t/p/w500/t6HIqrRAclMCA60NsSmeqe9RmNV.jpg"
-# )
-#
-# with app.app_context():
-#     db.session.add(second_movie)
-#     db.session.commit()
-
-
 @app.route("/")
 def home():
-    result = db.session.execute(db.select(Movie).order_by(Movie.rating))
+    # Sort movies by rating in descending order
+    result = db.session.execute(db.select(Movie).order_by(Movie.rating.desc()))
     all_movies = result.scalars().all()
-    for i in range(len(all_movies)):
-        all_movies[i].ranking = len(all_movies) - i
+
+    # Assign rankings based on the sorted order
+    for i, movie in enumerate(all_movies, start=1):
+        movie.ranking = i
     db.session.commit()
+
     return render_template("index.html", movies=all_movies)
 
 
